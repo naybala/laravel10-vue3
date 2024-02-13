b29e10e1a8185b9e8bf30be26021fe575233f2c5
Nay Ba La <naybala.zotefamily@gmail.com>
2024-02-13 23:28:22 +0630

    first commit

---
+++ resources/js/Components/Counter.vue
@@ -0,0 +1,14 @@
+<script setup>
+    import { ref } from 'vue'
+
+    const count = ref(0)
+</script>
+
+<template>
+    <h5>Ha ha</h5>
+    {{ count }}
+
+    <button @click="count++">
+        Add
+    </button>
+</template>
